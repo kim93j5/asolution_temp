e117aafdb988a7c8ec46e98ca99232e1ec8f7988
hj.Kim <hj.kim@asolution.biz>
2020-11-16 16:13:28 +0900

    hj.kim

---
+++ src/hjkim/Step1.java
@@ -0,0 +1,22 @@
+package hjkim;
+
+public class Step1 {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        print("test");
+
+        System.out.println(sum(2,5));
+    }
+
+    // Q1
+    public static void print(String param) {
+        System.out.println(param);
+    }
+
+    // Q2
+    public static int sum(int a, int b) {
+        return a+b;
+    }
+}
