a88a7558612c5d86c5d3877cfffacdc6528eb81a
jy.park <jy.park@asolution.biz>
2020-11-24 09:51:50 +0900

    step 1 - jy

---
+++ src/jypark/Step1.java
@@ -0,0 +1,143 @@
+package jypark;
+
+import java.util.Scanner;
+import java.util.*;
+
+public class Step1 {
+
+    public static void main(String[] args) {
+//     Q1();
+//     Q2();
+//     Q3();
+//     Q4();
+//     Q5();
+//     Q6();
+//     Q7();
+//     Q8();
+//     Q9();
+//     Q10();
+//     Q11();
+    }
+
+    // 01 Hello world
+    public static void Q1() {
+        System.out.print("Hello World");
+    }
+
+    // 02 We love kriii
+    public static void Q2() {
+        System.out.print("강한친구 대한육군");
+        System.out.print("강한친구 대한육군");
+    }
+
+    // 03 고양이
+    public static void Q3() {
+        System.out.print("\\    /\\\n");
+        System.out.print(" )  ( ')\n");
+        System.out.print("(  /  )\n");
+        System.out.print(" \\(__)|");
+    }
+
+    // 04 개
+    public static void Q4() {
+        System.out.println("|\\_/|     ");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\ ");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+
+    // 05 A+B
+    public static void Q5() {
+        int a, b;
+
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.print(a+b);
+    }
+
+    // 06 A-B
+    public static void Q6() {
+        int a,b;
+
+        Scanner sc = new Scanner((System.in));
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.print(a-b);
+    }
+
+    // 07 A*B
+    public static void Q7() {
+        int a,b;
+
+        Scanner sc = new Scanner((System.in));
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.print(a*b);
+    }
+
+    // 08 A/B
+    public static void Q8() {
+        double a,b;
+
+        Scanner sc = new Scanner((System.in));
+
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+
+        System.out.print(a*b);
+    }
+
+    // 09 사칙연산 ( A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램)
+    public static void Q9(){
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+
+    //10
+    public static void Q10(){
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println((a+b)%c);
+        System.out.println((a*b)%c);
+        System.out.println((a%c + b%c)%c);
+        System.out.println((a%c * b%c)%c);
+
+    }
+
+    //11
+    public static void Q11() {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        in.close();
+
+        System.out.println(a*(b%10));
+        System.out.println(a*(b%100/10));
+        System.out.println(a*(b/100));
+        System.out.println(a*b);
+    }
+
+}
